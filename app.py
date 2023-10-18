5a9ad7a290cfaab6f8c3e825e885343ddede52dd
miklderoche <127089400+miklderoche@users.noreply.github.com>
2023-10-18 17:56:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,29 @@
 import streamlit as st
 
+# Titre principal
 st.title('Cell Vision AI')
-st.write('Dans le domaine de la médecine et de la recherche biomédicale, l'analyse des cellules dans les frottis sanguins est d'une importance cruciale pour le diagnostic et la compréhension de nombreuses pathologies. Cependant, leur analyse manuelle est fastidieuse, sujette à des erreurs humaines, et peut être chronophage. De plus, cela nécessite de nombreux appareils relativement onéreux et l’utilisation de consommables potentiellement dangereux. Développer un outil pour automatiser la reconnaissance et la classification des différentes cellules sanguines permettrait de combler ces lacunes.')
-st.write('L’objectif principal du projet est la création d’algorithmes d’apprentissage machine et d’apprentissage profond dédiés à la reconnaissance et la classification de cellules du sang. Cet outil pourrait être utilisé pour faciliter le diagnostic de la leucémie en détectant des leucocytes anormaux.')
+
+# Introduction
+st.header('Introduction')
+st.write(
+    '''
+    Dans le domaine de la médecine et de la recherche biomédicale, l'analyse des cellules dans les frottis sanguins 
+    est d'une importance cruciale pour le diagnostic et la compréhension de nombreuses pathologies. Cependant, leur 
+    analyse manuelle est fastidieuse, sujette à des erreurs humaines, et peut être chronophage. De plus, cela 
+    nécessite de nombreux appareils relativement onéreux et l’utilisation de consommables potentiellement dangereux.
+    '''
+)
+
+# Objectif du projet
+st.header('Objectif du Projet')
+st.write(
+    '''
+    L’objectif principal du projet est la création d’algorithmes d’apprentissage machine et d’apprentissage profond 
+    dédiés à la reconnaissance et la classification de cellules du sang. Cet outil pourrait être utilisé pour 
+    faciliter le diagnostic de la leucémie en détectant des leucocytes anormaux.
+    '''
+)
+
+# Tu peux ajouter d'autres sections si nécessaire, par exemple:
+# st.header('Méthodologie')
+# st.write('...')
