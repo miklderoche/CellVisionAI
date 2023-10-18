c28cba8352cda0c33cdba478be29ee99a0c99295
miklderoche <127089400+miklderoche@users.noreply.github.com>
2023-10-19 00:08:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 left_co, cent_co,last_co = st.columns(3)
 with cent_co:
-    st.image('images/logo_VisionCellAI.png', width=250)
+    st.image('images/logo_VisionCellAI.png', width=300)
 
 
 # Titre principal
