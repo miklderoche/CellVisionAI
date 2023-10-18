6ca26eb37babb066b6f83a9977861b32629be92a
miklderoche <127089400+miklderoche@users.noreply.github.com>
2023-10-18 17:53:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
 st.title('Cell Vision AI')
-st.write('Bonjour,')
-st.write('Bienvenue dans mon application !')
+st.write('Dans le domaine de la médecine et de la recherche biomédicale, 
+l'analyse des cellules dans les frottis sanguins est d'une importance cruciale pour le diagnostic et la compréhension de nombreuses pathologies. 
+Cependant, leur analyse manuelle est fastidieuse, sujette à des erreurs humaines, et peut être chronophage. 
+De plus, cela nécessite de nombreux appareils relativement onéreux et l’utilisation de consommables potentiellement dangereux. 
+Développer un outil pour automatiser la reconnaissance et la classification des différentes cellules sanguines permettrait de combler ces lacunes.')
+st.write('L’objectif principal du projet est la création d’algorithmes d’apprentissage machine et d’apprentissage profond dédiés à la reconnaissance
+et la classification de cellules du sang. Cet outil pourrait être utilisé pour faciliter le diagnostic de la leucémie en détectant des leucocytes anormaux.
+')
