47818279b739d781c646adf778f5cb59f6be734f
miklderoche <127089400+miklderoche@users.noreply.github.com>
2023-10-18 23:34:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 import streamlit as st
 
-st.markdown(
-    "<img src='images/logo_VisionCellAI_2.png' width='50' height='50'>",
-    unsafe_allow_html=True
-)
+st.markdown("<img src='images/logo_VisionCellAI_2.png' width='50' height='50'>", unsafe_allow_html=True)
 
 # Titre principal
 st.title('Cell Vision AI')
