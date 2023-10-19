515cea30107af7cd09ce3d4a0f1f0521f015ec6a
miklderoche <127089400+miklderoche@users.noreply.github.com>
2023-10-19 09:22:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ def page_projet():
 # contenu de la page d'accueil...
 
 left_co, cent_co,last_co = st.columns(3)
-with cent_co:
-st.image('images/logo_VisionCellAI.png')
+    with cent_co:
+        st.image('images/logo_VisionCellAI.png')
 
 # Titre principal
-st.title('Cell Vision AI')
+    st.title('Cell Vision AI')
 
 # Introduction
-st.write(
+    st.write(
     '''
     Dans le domaine de la médecine et de la recherche biomédicale, l'analyse des cellules dans les frottis sanguins 
     est d'une importance cruciale pour le diagnostic et la compréhension de nombreuses pathologies. Cependant, leur 
@@ -24,22 +24,22 @@ st.write(
 )
 
 # Objectif du projet
-st.header('Objectif du Projet')
-st.write(
+    st.header('Objectif du Projet')
+    st.write(
     '''
     L’objectif principal du projet est la création d’algorithmes d’apprentissage machine et d’apprentissage profond 
     dédiés à la reconnaissance et la classification de cellules du sang. Cet outil pourrait être utilisé pour 
     faciliter le diagnostic de la leucémie en détectant des leucocytes anormaux.
     '''
-)
+    )
 
 # Equipe du projet
-st.write(
+    st.write(
     '''
     **Projet réalisé par Wilfried Condemine, Michael Deroche, Claudia Mattei et Charles Sallard**.
     (Promotion sept23_DS Datascientest)
     '''
-)
+    )
 
 def page_demo():
     st.title("Démonstration")
