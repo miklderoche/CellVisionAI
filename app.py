f53f190ddaef911bf36685b796ab011b7be015fc
miklderoche <127089400+miklderoche@users.noreply.github.com>
2023-10-19 17:15:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,32 +13,33 @@ def page_accueil():
 # Titre principal
     st.title('Cell Vision AI')
 
-    # Onglet "Introduction"
-    with st.tabs(["Introduction", "Objectif du Projet", "Equipe du projet"]):
-        if st.tab_selection == "Introduction":
-            st.write(
-                '''
-                Dans le domaine de la médecine et de la recherche biomédicale, l'analyse des cellules dans les frottis sanguins 
-                est d'une importance cruciale pour le diagnostic et la compréhension de nombreuses pathologies. Cependant, leur 
-                analyse manuelle est fastidieuse, sujette à des erreurs humaines, et peut être chronophage. De plus, cela 
-                nécessite de nombreux appareils relativement onéreux et l’utilisation de consommables potentiellement dangereux.
-                '''
-            )
-        elif st.tab_selection == "Objectif du Projet":
-            st.write(
-                '''
-                L’objectif principal du projet **CellVisionAI** est la création d’algorithmes d’apprentissage machine et d’apprentissage profond 
-                dédiés à la reconnaissance et la classification de cellules du sang. Cet outil pourrait être utilisé pour 
-                faciliter le diagnostic de la leucémie en détectant des leucocytes anormaux.
-                '''
-            )
-        else:
-            st.write(
-                '''
-                **Projet réalisé par Wilfried Condemine, Michael Deroche, Claudia Mattei et Charles Sallard**.
-                (Promotion sept23_DS Datascientest)
-                '''
-            )
+# Introduction
+    st.write(
+    '''
+    Dans le domaine de la médecine et de la recherche biomédicale, l'analyse des cellules dans les frottis sanguins 
+    est d'une importance cruciale pour le diagnostic et la compréhension de nombreuses pathologies. Cependant, leur 
+    analyse manuelle est fastidieuse, sujette à des erreurs humaines, et peut être chronophage. De plus, cela 
+    nécessite de nombreux appareils relativement onéreux et l’utilisation de consommables potentiellement dangereux.
+    '''
+    )
+
+# Objectif du projet
+    st.header('Objectif du Projet')
+    st.write(
+    '''
+    L’objectif principal du projet **CellVisionAI** est la création d’algorithmes d’apprentissage machine et d’apprentissage profond 
+    dédiés à la reconnaissance et la classification de cellules du sang. Cet outil pourrait être utilisé pour 
+    faciliter le diagnostic de la leucémie en détectant des leucocytes anormaux.
+    '''
+    )
+
+# Equipe du projet
+    st.write(
+    '''
+    **Projet réalisé par Wilfried Condemine, Michael Deroche, Claudia Mattei et Charles Sallard**.
+    (Promotion sept23_DS Datascientest)
+    '''
+    )
 
 def page_projet():
     # contenu de la page démonstration...
