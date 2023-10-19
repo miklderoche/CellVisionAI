b2acce824e8afa5e56c1f6ed2603b36459dc6848
miklderoche <127089400+miklderoche@users.noreply.github.com>
2023-10-19 09:18:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,81 @@
 import streamlit as st
-        
-left_co, cent_co,last_co = st.columns(3)
-with cent_co:
-    st.image('images/logo_VisionCellAI.png')
+
+# %% (_.~" MENU BARRE LATERALE "~._) 
+
+# Menu latéral
+st.sidebar.title("Navigation")
+pages = {
+    "Projet": page_projet,
+    "Démonstration": page_demo,
+    "Résultats": page_resultats,
+    "Documentation": page_docs,
+}
+
+selection = st.sidebar.radio("Sélectionner une page", list(pages.keys()))
+
+# Affichage de la page en fonction de la sélection
+pages[selection]()
+
+# %% (_.~" PAGES "~._) 
+
+# Fonctions pour chaque page
+def page_projet():
+    st.title("Projet")    
+    # contenu de la page d'accueil...
+
+    left_co, cent_co,last_co = st.columns(3)
+    with cent_co:
+        st.image('images/logo_VisionCellAI.png')
 
 # Titre principal
-st.title('Cell Vision AI')
+    st.title('Cell Vision AI')
 
 # Introduction
-st.write(
+    st.write(
     '''
     Dans le domaine de la médecine et de la recherche biomédicale, l'analyse des cellules dans les frottis sanguins 
     est d'une importance cruciale pour le diagnostic et la compréhension de nombreuses pathologies. Cependant, leur 
     analyse manuelle est fastidieuse, sujette à des erreurs humaines, et peut être chronophage. De plus, cela 
     nécessite de nombreux appareils relativement onéreux et l’utilisation de consommables potentiellement dangereux.
     '''
-)
+    )
 
 # Objectif du projet
-st.header('Objectif du Projet')
-st.write(
+    st.header('Objectif du Projet')
+    st.write(
     '''
     L’objectif principal du projet est la création d’algorithmes d’apprentissage machine et d’apprentissage profond 
     dédiés à la reconnaissance et la classification de cellules du sang. Cet outil pourrait être utilisé pour 
     faciliter le diagnostic de la leucémie en détectant des leucocytes anormaux.
     '''
-)
+    )
 
 # Equipe du projet
-st.write(
+    st.write(
     '''
     **Projet réalisé par Wilfried Condemine, Michael Deroche, Claudia Mattei et Charles Sallard**.
     (Promotion sept23_DS Datascientest)
     '''
-)
+    )
+
+def page_demo():
+    st.title("Démonstration")
+    st.write("Voici la page de démonstration.")
+    # contenu de la page démonstration...
+
+def page_resultats():
+    st.title("Résultats")
+    # contenu de la page résultats...
+
+def page_docs():
+    st.title("Documentation")
+    # contenu de la page Documentation...
+
+# Menu latéral
+st.sidebar.title("Navigation")
+selection = st.sidebar.radio("Aller à", list(pages.keys()))
+
+# Affichage de la page en fonction de la sélection
+page = pages[selection]
+page()
+
