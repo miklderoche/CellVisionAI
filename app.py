3cd7232efb18dfe41853fcfc183942a0ff8d1638
miklderoche <127089400+miklderoche@users.noreply.github.com>
2023-10-19 14:20:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ if 'page' not in st.session_state:
     st.session_state['page'] = 'Accueil'
 
 # Image en haut du menu latéral
-st.sidebar.image('images/logo_VisionCellAI.png', width="50%")
+st.sidebar.image('images/logo_VisionCellAI.png', width="100")
 
 # Menu latéral avec des boutons
 if st.sidebar.button('Accueil'):
