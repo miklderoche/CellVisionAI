607ba1ad21f22f3714781bb48b8f23257ed0e9a8
miklderoche <127089400+miklderoche@users.noreply.github.com>
2023-10-19 09:24:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ pages = {
     "Documentation": page_docs,
 }
 
-selection = st.sidebar.st.selectbox("Sélectionner une page", list(pages.keys()))
+selection = st.selectbox("Sélectionner une page", list(pages.keys()))
 
 # Affichage de la page en fonction de la sélection
 pages[selection]()
