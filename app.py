057e4db52e60fff0641a8bbb1d69a9587aa33cf6
miklderoche <127089400+miklderoche@users.noreply.github.com>
2023-10-20 17:50:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -134,9 +134,9 @@ elif st.session_state['page'] == 'Documentation':
 
     with tab5:
         st.write('''**Données** :''')
-        st.markdown('''https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7182702/''')
-        st.markdown('''https://www.kaggle.com/eugeneshenderov/acute-promyelocytic-leukemia-apl''')
-        st.markdown('''https://www.kaggle.com/nikhilsharma00/leukemia-dataset''')
+        st.markdown('''- https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7182702/''')
+        st.markdown('''- https://www.kaggle.com/eugeneshenderov/acute-promyelocytic-leukemia-apl''')
+        st.markdown('''- https://www.kaggle.com/nikhilsharma00/leukemia-dataset''')
     
     with tab6:
         st.write(
