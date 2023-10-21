df2e806dde4b31bddbdde0b752f63160dbf5489b
miklderoche <127089400+miklderoche@users.noreply.github.com>
2023-10-21 20:30:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -209,7 +209,7 @@ elif st.session_state['page'] == 'Analyse':
         
         # Mettre à jour la mise en page pour ajuster la taille et mettre un fond transparent
         fig_dimensions_largeur.update_layout(
-            width=300,  # Ajustez la largeur
+            width=325,  # Ajustez la largeur
             height=400,  # Ajustez la hauteur
             paper_bgcolor='rgba(0,0,0,0)',
             plot_bgcolor='rgba(0,0,0,0)',
