f1b3c31c6ec3a411e524b6151f749e6bffcbb73f
miklderoche <127089400+miklderoche@users.noreply.github.com>
2023-10-21 23:48:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,8 @@ if st.sidebar.button('Documentation'):
 
 # Affichage de la page en fonction de l'état de la session
 if st.session_state['page'] == 'Accueil':
-    with:
+    cent_co = st.columns(1)
+    with cent_co:
         st.image('images/illustration_accueil.png', width=500)
 
     st.title('Cell Vision AI')
