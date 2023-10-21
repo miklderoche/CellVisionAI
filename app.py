0af4b0715f7dc7bb12b636889d1bd26465bd1767
miklderoche <127089400+miklderoche@users.noreply.github.com>
2023-10-21 11:37:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ elif st.session_state['page'] == 'Analyse':
             text=data['Nombre d\'images'], 
             marker_color=colors[:len(data)],  # Applique les couleurs aux barres
             textposition='inside',
-            insidetextorientation='horizontal'
+            insidetextanchor='horizontal'
         )])
 
         # Mettre à jour la mise en page pour ajuster la taille et mettre un fond transparent
