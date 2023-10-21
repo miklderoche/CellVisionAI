e0350dcb94cbeaf153a16f33209119663223994a
miklderoche <127089400+miklderoche@users.noreply.github.com>
2023-10-21 11:19:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -118,8 +118,8 @@ elif st.session_state['page'] == 'Analyse':
 
         # Mettre à jour la mise en page pour ajuster la taille et mettre un fond transparent
         fig_bar.update_layout(
-            width=400,
-            height=400,
+            width=550,
+            height=450,
             paper_bgcolor='rgba(0,0,0,0)',
             plot_bgcolor='rgba(0,0,0,0)',
             title='Distribution des types de cellules dans le dataset',
@@ -137,8 +137,8 @@ elif st.session_state['page'] == 'Analyse':
 
         # Mettre à jour la mise en page pour ajuster la taille et mettre un fond transparent
         fig_pie.update_layout(
-            width=550,
-            height=500,
+            width=400,
+            height=400,
             paper_bgcolor='rgba(0,0,0,0)',
             plot_bgcolor='rgba(0,0,0,0)',
             title='Proportions des types de cellules dans le dataset'
