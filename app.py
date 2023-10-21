30a59dac2ed8dc81549d11e767cbf414cdbe2431
miklderoche <127089400+miklderoche@users.noreply.github.com>
2023-10-21 11:09:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -118,8 +118,8 @@ elif st.session_state['page'] == 'Analyse':
         
         # Mettre à jour la mise en page pour ajuster la taille et mettre un fond transparent
         fig.update_layout(
-            width=400,
-            height=450,
+            width=450,
+            height=600,
             paper_bgcolor='rgba(0,0,0,0)',
             plot_bgcolor='rgba(0,0,0,0)',
             title='Distribution des types de cellules dans le dataset',
