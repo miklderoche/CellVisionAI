c1ba73050a41c27c0e74379a4db673c83b97dc6a
miklderoche <127089400+miklderoche@users.noreply.github.com>
2023-10-23 15:08:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -352,31 +352,26 @@ elif st.session_state['page'] == 'Analyse':
         df_data_leukemia_dataset['Statut patient'] = df_data_leukemia_dataset['Leucémie_ALL'].apply(lambda x: 'Malade' if x == 1 else 'Sain')
         
         # Afficher la distribution des classes en distinguant les patients sains et malades
-        st.write('**Distribution des classes ALL_IDB1 et ALL_IDB2 en distinguant les patients sains et malades**')
         fig1 = px.histogram(df_data_leukemia_dataset, x='Classe', color='Statut patient')
-        fig1.update_layout(title={'text': 'Distribution des classes ALL_IDB1 et ALL_IDB2 en distinguant les patients sains et malades', 'y':0.95, 'x':0.5})
+        fig1.update_layout(title={'text': 'Distribution des classes ALL_IDB1 et ALL_IDB2 en distinguant les patients sains et malades'})
         st.plotly_chart(fig1)
         
         # Afficher la distribution de la dimension pour les classes ALL_IDB1 et ALL_IDB2
-        st.write('**Distribution de la dimension pour les classes ALL_IDB1 et ALL_IDB2**')
         fig2 = px.histogram(df_data_leukemia_dataset, x='Dimensions', color='Classe')
         fig2.update_layout(title={'text': 'Distribution de la dimension pour les classes ALL_IDB1 et ALL_IDB2', 'y':0.95, 'x':0.5})
         st.plotly_chart(fig2)
         
         # Afficher la distribution de la résolution pour les classes ALL_IDB1 et ALL_IDB2
-        st.write('**Distribution de la résolution pour les classes ALL_IDB1 et ALL_IDB2**')
         fig3 = px.histogram(df_data_leukemia_dataset, x='Résolution', color='Classe')
         fig3.update_layout(title={'text': 'Distribution de la résolution pour les classes ALL_IDB1 et ALL_IDB2', 'y':0.95, 'x':0.5})
         st.plotly_chart(fig3)
         
         # Ajouter une boîte à moustaches pour la luminosité
-        st.write('**Distribution de la luminosité pour les classes ALL_IDB1 et ALL_IDB2**')
         fig4 = px.box(df_data_leukemia_dataset, x='Classe', y='Luminosité', color='Classe')
         fig4.update_layout(title={'text': 'Distribution de la luminosité pour les classes ALL_IDB1 et ALL_IDB2', 'y':0.95, 'x':0.5})
         st.plotly_chart(fig4)
         
         # Ajouter une boîte à moustaches pour la teinte
-        st.write('**Distribution de la teinte pour les classes ALL_IDB1 et ALL_IDB2**')
         fig5 = px.box(df_data_leukemia_dataset, x='Classe', y='Teinte', color='Classe')
         fig5.update_layout(title={'text': 'Distribution de la teinte pour les classes ALL_IDB1 et ALL_IDB2', 'y':0.95, 'x':0.5})
         st.plotly_chart(fig5)
